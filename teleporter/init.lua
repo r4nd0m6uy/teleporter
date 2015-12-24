f871c51c4337f3779340f0a0532fc34c857249fe
Random Guy <morandg@gmail.com>
2015-12-24 16:21:46 +0100

    Add basic UI idea

--- teleporter/init.lua
+++ teleporter/init.lua
@@ -1,6 +1,6 @@
 ---
---Teleporter 1.07
---Copyright (C) 2012 Bad_Command
+--Teleporter next
+--Copyright (C) 2015 R4nd0m6uy
 --
 --This library is free software; you can redistribute it and/or
 --modify it under the terms of the GNU Lesser General Public
@@ -18,184 +18,264 @@
 ----
 
 teleporter = {}
-teleporter.version = 1.07
+teleporter.version = 0.0
+teleporter.db = nil
 
 -- config.lua contains configuration parameters
 dofile(minetest.get_modpath("teleporter").."/config.lua")
 
-minetest.register_craft({
-	output = 'teleporter:teleporter_pad',
-	recipe = {
-                {'moreores:copper_ingot', 'default:glass', 'moreores:copper_ingot'},
-                {'moreores:copper_ingot', 'moreores:gold_block', 'moreores:copper_ingot'},
-                {'moreores:copper_ingot', 'mesecons_powerplant:power_plant', 'moreores:copper_ingot'},
-        }
-})
+------------------------------------------------------------------------
+-- Teleporters database
+------------------------------------------------------------------------
+local function build_hash(pos)
+        return ""..pos.x.."/"..pos.y.."/"..pos.z
+end
 
-minetest.register_craft({
-	output = 'teleporter:teleporter_pad',
-	recipe = {
-                {'default:wood', 'default:glass', 'default:wood'},
-                {'default:wood', 'default:mese', 'default:wood'},
-                {'default:wood', 'default:wood', 'default:wood'},
-        }
-})
+local function get_teleporters_db()
+        if teleporter.db == nil
+        then
+                -- TODO : read file
+                
+                teleporter.db = {}
+        end
+        
+        return teleporter.db
+end
 
+local function get_teleporter_at(pos)
+        local db = get_teleporters_db()
+        local hash = build_hash(pos)
+        
+        if db[hash] == nil
+        then
+                db[hash] = {}
+        end
+        
+        return db[hash]
+end
+
+local function get_teleporter_hash_from_name(name)
+        local db = get_teleporters_db()
+        
+        for hash, tp in pairs(db) do                
+                if db[hash].name == name
+                then
+                        return hash
+                end
+        end
+end
+
+local function teleporter_name_exists(name)
+        local db = get_teleporters_db()
+        
+        for hash, tp in pairs(db) do                
+                if db[hash].name == name
+                then
+                        return true
+                end
+        end
+        
+        return false
+end
+
+------------------------------------------------------------------------
+-- Update world
+------------------------------------------------------------------------
+local function build_destination_drop_list(source_hash)
+        local destination_list = ""
+        local destination_sel_idx = 1
+        local crt_sel_idx = 2
+        local db = get_teleporters_db()
+        
+        for dest_hash, dest_tp in pairs(db) do
+                -- TODO Check if allowed to teleport to this destination
+                if dest_hash ~= source_hash
+                then
+                        if destination_list == ""
+                        then
+                                destination_list = ","..dest_tp.name
+                        else
+                                destination_list = destination_list..","..dest_tp.name
+                        end
+                        
+                        -- Preselected destination
+                        if db[source_hash].destination_hash == dest_hash
+                        then
+                                destination_sel_idx = crt_sel_idx
+                        end
+                        
+                        crt_sel_idx = crt_sel_idx  + 1
+                end
+        end
+        
+        return "dropdown[1,2;4,1;dest;"..destination_list..";"..destination_sel_idx.."]"
+end
+
+local function update_teleporters_meta()
+        local meta
+        local db = get_teleporters_db()
+        
+        for hash, tp in pairs(db) do
+                meta = minetest.env:get_meta(tp.location)
+                
+                -- Check if got unlinked
+                if tp.destination_hash ~= nil and db[tp.destination_hash] == nil
+                then
+                        tp.destination_hash = nil
+                end
+                
+                -- Info text
+                if tp.destination_hash == nil
+                then
+                        meta:set_string("infotext", tp.name..": unlinked")
+                else
+                        meta:set_string("infotext", tp.name..": linked to "..db[tp.destination_hash].name)
+                end
+                
+                -- Build right click menu
+                meta:set_string("formspec",
+                        "size[5,3]"..
+                        "label[2,0;Configure teleporter]"..
+                        "field[1,1;4,1;ID;Teleporter name;"..tp.name.."]"..
+                        "label[0,2;Destination]"..
+                        build_destination_drop_list(hash))
+        end
+end
+
+------------------------------------------------------------------------
+-- Configure teleporters
+------------------------------------------------------------------------
+local function teleporter_configured(pos, formname, fields, sender)
+        local teleporter = get_teleporter_at(pos)
+        local db = get_teleporters_db()
+        local newName = fields["ID"]
+        local newDest = fields["dest"]
+        local player_name = sender:get_player_name()
+        
+        -- TODO Check if allowed to configured this teleporter
+        
+        -- Name was changed
+        if newName ~= nil and newName ~= "" and newName ~= teleporter.name
+        then
+                if teleporter_name_exists(newName)
+                then
+                        minetest.chat_send_player(player_name, "A teleporter named '"..newName.."' already exists")
+                else
+                        teleporter.name = newName
+                end
+        end        
+        
+        -- destination was changed
+        if newDest ~= nil and newDest ~= teleporter.destination_name
+        then                
+                if newDest == "" then
+                        minetest.log("Teleporter deactivated by user")
+                        
+                        teleporter.destination_hash = nil                        
+                        -- TODO Lighting effect off
+                else
+                        minetest.log("New "..teleporter.name.." teleports to "..newDest)
+                        
+                        teleporter.destination_hash = get_teleporter_hash_from_name(newDest)                        
+                        -- TODO Lighting effect on
+                end
+        end
+        
+        update_teleporters_meta()
+end
+
+------------------------------------------------------------------------
+-- on delete
+------------------------------------------------------------------------
+local function teleporter_destructed(pos)        
+        get_teleporters_db()[build_hash(pos)] = nil;        
+        update_teleporters_meta()
+end
+
+------------------------------------------------------------------------
+-- on_construct
+------------------------------------------------------------------------
+local function teleporter_constructed(pos, isPublic)
+        local db = get_teleporters_db()
+        local hash = build_hash(pos)
+        local teleporter = {
+                name = hash,
+                location = pos,
+                public = isPublic,
+                destination_hash = nil,
+                owner = nil,
+        }
+        
+        db[hash] = teleporter
+        update_teleporters_meta()
+end
+
+local function teleporter_pad_constructed(pos)
+     teleporter_constructed(pos, 1)   
+end
+
+local function teleporter_pad_private_constructed(pos)
+     teleporter_constructed(pos, 0)   
+end
+
+------------------------------------------------------------------------
+-- minetest.register_node
+------------------------------------------------------------------------
 minetest.register_node("teleporter:teleporter_pad", {
-	tile_images = {teleporter.tile_image},
+	tile_images = {"teleporter_teleporter_pad.png"},
 	drawtype = "signlike",
 	paramtype = "light",
 	paramtype2 = "wallmounted",
 	walkable = false,
 	description="Teleporter Pad",
-	inventory_image = teleporter.tile_image,
+	inventory_image = "teleporter_teleporter_pad.png",
 	metadata_name = "sign",
-	--sounds = default.node_sound_defaults(),
 	groups = {choppy=2,dig_immediate=2},
 	selection_box = {
 		type = "wallmounted",
 	},
-        on_construct = function(pos)
-                local meta = minetest.env:get_meta(pos)
-                meta:set_string("formspec", "hack:sign_text_input")
-                meta:set_string("infotext", "\"Teleport to "..teleporter.default_coordinates.desc.."\"")
-		meta:set_string("text", teleporter.default_coordinates.x..","..
-			teleporter.default_coordinates.y..","..
-			teleporter.default_coordinates.z..","..
-			teleporter.default_coordinates.desc)
-		meta:set_float("enabled", -1)
-		meta:set_float("x", teleporter.default_coordinates.x)
-		meta:set_float("y", teleporter.default_coordinates.y)
-		meta:set_float("z", teleporter.default_coordinates.z)
-        end,
-	after_place_node = function(pos, placer)
-		local meta = minetest.env:get_meta(pos)
-		local name = placer:get_player_name()
-		meta:set_string("owner", name)
-		
-		if teleporter.perms_to_build and not minetest.get_player_privs(name)["teleport"] then
-			minetest.chat_send_player(name, 'Teleporter:  Teleport privileges are required to build teleporters.')
-			minetest.env:remove_node(pos)
-			minetest.env:add_item(pos, 'teleporter:teleporter_pad')
-		else
-			meta:set_float("enabled", 1)
-		end
-
-	end,
-        on_receive_fields = function(pos, formname, fields, sender)
-		local coords = teleporter.coordinates(fields.text)
-                local meta = minetest.env:get_meta(pos)
-		local name = sender:get_player_name()
-		local privs = minetest.get_player_privs(name)
-
-		if name ~= meta:get_string("owner") and not privs["server"] then
-			minetest.chat_send_player(name, 'Teleporter:  This is not your teleporter, it belongs to '..meta:get_string("owner"))
-			return false
-		else if privs["server"] then
-			minetest.chat_send_player(name, 'Teleporter:  This teleporter belongs to '..meta:get_string("owner"))
-		end
-
-		if teleporter.perms_to_configure and not privs["teleport"] then
-			minetest.chat_send_player(name, 'Teleporter:  You need teleport privileges to configure a teleporter')
-			return
-		end
-
-		local infotext = ""
-		if coords~=nil then	
-			meta:set_float("x", coords.x)
-			meta:set_float("y", coords.y)
-			meta:set_float("z", coords.z)
-			if teleporter.requires_pairing and not teleporter.is_paired(coords) and not privs["server"] then
-				minetest.chat_send_player(name, 'Teleporter:  There is no recently-used teleporter pad at the destination!')
-		                meta:set_string("text", fields.text)
-				infotext="Teleporter is Disabled"
-				meta:set_float("enabled", -1)
-			else
-				meta:set_float("enabled", 1)
-				if coords.desc~=nil then
-					infotext="Teleport to "..coords.desc
-				else
-					infotext="Teleport to "..coords.x..","..coords.y..","..coords.z..""
-				end
-			end
-		else
-			minetest.chat_send_player(name, 'Teleporter:  Incorrect coordinates.  Enter them as \'X,Y,Z,Description\' without decimals.')
-			meta:set_float("enabled", -1)
-			infotext="Teleporter Offline"
-		end
-
-                print((sender:get_player_name() or "").." entered \""..fields.text..
-                                "\" to teleporter at "..minetest.pos_to_string(pos))
-                meta:set_string("text", fields.text)
-                meta:set_string("infotext", '"'..infotext..'"')
-        	end
-	end,
-	can_dig = function(pos,player)
-		local meta = minetest.env:get_meta(pos)
-		local name = player:get_player_name()
-		local privs = minetest.get_player_privs(name)
-		if name == meta:get_string("owner") or privs["server"] then
-			return true
-		end
-		return false
-	end
+        on_construct = teleporter_pad_constructed,
+        on_receive_fields = teleporter_configured,
+        on_destruct = teleporter_destructed
 })
 
-teleporter.is_paired = function(coords) 
-	for dx=-teleporter.pairing_check_radius,teleporter.pairing_check_radius do
-		for dy=-teleporter.pairing_check_radius,teleporter.pairing_check_radius do
-			for dz=-teleporter.pairing_check_radius,teleporter.pairing_check_radius do
-				local node = minetest.env:get_node({x=coords.x + dx, y=coords.y + dy, z=coords.z + dz})
-				if node.name == 'teleporter:teleporter_pad' then
-					return true
-				end
-			end
-		end
-	end
-	return false
-end
-
-teleporter.coordinates = function(str) 
-	local x,y,z,desc = string.match(str, "^(-?%d+),(-?%d+),(-?%d+),?(.*)$")
-	
-	if desc=="" then
-		desc = nil
-	end
-
-	if x==nil or y==nil or z==nil or 
-		string.len(x) > 6 or string.len(y) > 6 or string.len(z) > 6 then
-			return nil
-	end
-
-	x = x + 0.0
-	y = y + 0.0
-	z = z + 0.0
-
-	if x > 32765 or x < -32765 or y > 32765 or y < -32765 or z > 32765 or z < -32765 then
-		return nil
-	end
-
-	return {x=x, y=y, z=z, desc=desc}
-end
-
-
-minetest.register_abm(
-	{nodenames = {"teleporter:teleporter_pad"},
-	interval = 1.0,
-	chance = 1,
-	action = function(pos, node, active_object_count, active_object_count_wider)
-		local objs = minetest.env:get_objects_inside_radius(pos, 1)
-		for k, player in pairs(objs) do
-			if player:get_player_name()~=nil then 
-				local meta = minetest.env:get_meta(pos)
-				if meta:get_float("enabled") > 0 then
-					local target_coords={x=meta:get_float("x"), y=meta:get_float("y"), z=meta:get_float("z")}
-					minetest.sound_play("teleporter_teleport", {pos = pos, gain = 1.0, max_hear_distance = 10,})
-					player:moveto(target_coords, false)
-					minetest.sound_play("teleporter_teleport", {pos = target_coords, gain = 1.0, max_hear_distance = 10,})
-				end
-			end
-		end
-	end	
+minetest.register_node("teleporter:teleporter_pad_private", {
+	tile_images = {"teleporter_teleporter_pad_private.png"},
+	drawtype = "signlike",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	walkable = false,
+	description="Teleporter Pad",
+	inventory_image = "teleporter_teleporter_pad_private.png",
+	metadata_name = "sign",
+	groups = {choppy=2,dig_immediate=2},
+	selection_box = {
+		type = "wallmounted",
+	},
+        on_construct = teleporter_pad_private_constructed,
+        on_receive_fields = teleporter_configured,
+        on_destruct = teleporter_destructed
 })
+
+------------------------------------------------------------------------
+-- minetest.register_craft
+------------------------------------------------------------------------
+minetest.register_craft({
+	output = 'teleporter:teleporter_pad',
+	recipe = {
+                {'default:glass', 'default:glass', 'default:glass'},
+                {'', '', ''},
+                {'', 'default:mese', ''},
+        }
+})
+
+minetest.register_craft({
+	output = 'teleporter:teleporter_pad_private',
+	recipe = {
+                {'default:glass', 'default:glass', 'default:glass'},
+                {'', 'default:steel_ingot', ''},
+                {'', 'default:mese', ''},
+        }
+})
+
 
